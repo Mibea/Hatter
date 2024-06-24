a57ed736e49f964be47d5459a3521497c18453ae
Mibea <96521634+Mibea@users.noreply.github.com>
2024-06-24 20:40:43 +0200

    Wallpaper

---
+++ Firefox theme/user.js
@@ -0,0 +1,7 @@
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Enable rounded bottom window corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
